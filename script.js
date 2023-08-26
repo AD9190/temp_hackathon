8c920f4724566b4aff4d6cb5064ee0bc022d33b9
Debaditya Som <121785700+Debaditya-Som@users.noreply.github.com>
2023-08-26 19:08:52 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+const historyImages = document.querySelectorAll('.history-image img');
+let currentImageIndex = 0;
+
+function changeHistoryImage() {
+    historyImages[currentImageIndex].classList.remove('active');
+    currentImageIndex = (currentImageIndex + 1) % historyImages.length;
+    historyImages[currentImageIndex].classList.add('active');
+}
+
+setInterval(changeHistoryImage, 3000); // Change image every 3 seconds
